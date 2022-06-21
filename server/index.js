fea1aec1fffe59933c21878cf7b78a9372364972
theandersonn <theandersonn@gmail.com>
2022-06-21 10:03:11 -0300

    server: conexao com o banco

--- server/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.listen(process.env.PORT);
---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+
+const connectDB = require('./db/connect');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+const startApp = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(
+      process.env.PORT,
+      console.log(`🚀 Server is listening on port ${process.env.PORT}...`),
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
